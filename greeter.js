051da08ade8cae2f92f6a4776de7b1f4dbd18f24
klrpxy <klrpxy@163.com>
2026-05-16 13:17:53 +0800

    🧪 test(类构造器的public入参):

--- greeter.js
+++ greeter.js
@@ -1,6 +1,19 @@
 "use strict";
-function gretter(person) {
+// TypeScript 中对象实现接口时只需包含接口要求的结构即可，不必明确显示实现。
+class Student {
+    firstName;
+    middleInitial;
+    lastName;
+    fullName;
+    constructor(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+    }
+}
+function greeter(person) {
     return `Hello, ${person.firstName} ${person.lastName}`;
 }
-let user = { firstName: "Jame", lastName: "User" };
-document.body.innerHTML = gretter(user);
+let user = new Student('Jame', 'M.', 'User');
+document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,14 +1,25 @@
-// TypeScript 中对象实现接口时只需包含接口要求的结构即可，不必明确显示实现。
+// TypeScript 类构造器的入参支持添加public来声明为类成员
+
+class Student {
+    fullName: string;
+    constructor(
+        public firstName: string,
+        public middleInitial: string,
+        public lastName: string,
+    ) {
+        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+    }
+}
 
 interface Person {
     firstName: string;
     lastName: string;
 }
 
-function gretter(person: Person) {
+function greeter(person: Person) {
     return `Hello, ${person.firstName} ${person.lastName}`;
 }
 
-let user = { firstName: 'Jame', lastName: 'User' };
+let user = new Student('Jame', 'M.', 'User');
 
-document.body.innerHTML = gretter(user);
+document.body.innerHTML = greeter(user);
